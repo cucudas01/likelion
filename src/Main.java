34f94454009f99dab6756ae5a64cc95de3464c1f
cucudas01 <4996yys@naver.com>
2026-04-01 17:08:15 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, LikeLion!");
         System.out.println("Nice to meet you");
+        System.out.println("have a nice day");
+
     }
 }
